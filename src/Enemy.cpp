2f40be62e1b66278b90008abc34f370684f85bfa
Clint Bellanger <clintbellanger@gmail.com>
2011-06-14 16:16:31 -0500

    Integer divide on life/mana steal

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -511,11 +511,11 @@ bool Enemy::takeHit(Hazard h) {
 			if (h.bleed_duration > stats.bleed_duration) stats.bleed_duration = h.bleed_duration;
 			if (h.immobilize_duration > stats.immobilize_duration) stats.immobilize_duration = h.immobilize_duration;
 			if (h.hp_steal != 0) {
-				h.src_stats->hp += ceil(dmg * (h.hp_steal / 100.0));
+				h.src_stats->hp += ceil((dmg * h.hp_steal) / 100);
 				if (h.src_stats->hp > h.src_stats->maxhp) h.src_stats->hp = h.src_stats->maxhp;
 			}
 			if (h.mp_steal != 0) {
-				h.src_stats->mp += ceil(dmg * (h.mp_steal / 100.0));
+				h.src_stats->mp += ceil((dmg * h.mp_steal) / 100);
 				if (h.src_stats->mp > h.src_stats->maxmp) h.src_stats->mp = h.src_stats->maxmp;
 			}
 		}
