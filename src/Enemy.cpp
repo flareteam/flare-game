e8692134ca3e9edcaaf48b231af633ee328ed214
Thane Brimhall <thane.brimhall@gmail.com>
2011-07-11 16:21:43 -0600

    Refactored post_power and wall_power to use caster as source

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -533,7 +533,7 @@ bool Enemy::takeHit(Hazard h) {
 		Point pt;
 		pt.x = pt.y = 0;
 		if (h.post_power >= 0 && dmg > 0) {
-			powers->activate(h.post_power, &stats, pt);
+			powers->activate(h.post_power, h.src_stats, stats.pos);
 		}
 		
 		// interrupted to new state
