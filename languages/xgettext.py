52f0cafa6e380350e413b76cbd80d89bc2dfd989
Thomas Glamsch (CruzR) <thomas-glamsch@gmx.de>
2011-11-03 02:45:12 +0100

    Introduced various changes to xgettext.py: * Fixed bug #148 * Now escapes double quotes * Now potentially supports I18n of quests, npcs and maps. Didn't know whether this is wanted, so I decided to comment the code.

--- languages/xgettext.py
+++ languages/xgettext.py
@@ -2,7 +2,7 @@
 import os
 import datetime
 
-keys = set()
+keys = []
 now = datetime.datetime.now()
 header = r'''# Copyright (C) 2011 Clint Bellanger
 # This file is distributed under the same license as the FLARE package.
@@ -28,27 +28,53 @@ def extract(filename):
     infile = open(filename, 'r')
     for line in infile.readlines():
         if line.startswith('name='):
-            keys.add(line[5:].strip('\n'))
-        elif line.startswith('power_desc='):
-            keys.add(line[11:].strip('\n'))
+            keys.append(line[5:].strip('\n').replace("\"", "\\\""))
+        elif line.startswith('power_desc=') or line.startswith('quest_text='):
+            keys.append(line[11:].strip('\n').replace("\"", "\\\""))
         elif line.startswith('description='):
-            keys.add(line[12:].strip('\n'))
+            keys.append(line[12:].strip('\n').replace("\"", "\\\""))
         elif line.startswith('title='):
-            keys.add(line[6:].strip('\n'))
-        elif line.startswith('msg='):
-            keys.add(line[4:].strip('\n'))
+            keys.append(line[6:].strip('\n').replace("\"", "\\\""))
+        elif line.startswith('msg=') or line.startswith('him=') or line.startswith('you='):
+            keys.append(line[4:].strip('\n').replace("\"", "\\\""))
+        elif line.startswith('tooltip='):
+            keys.append(line[8:].strip('\n').replace("\"", "\\\""))
+
+# this removes duplicates from keys in a clean way (without screwing it up)
+def remove_duplicates():
+    global keys
+    tmp = []
+    for node in keys:
+        if node not in tmp:
+            tmp.append(node)
+    keys = tmp
 
 # this writes the list of keys to a gettext .po file
 def save(filename):
     outfile = open('data.pot', 'w')
     outfile.write(header.format(now=now.strftime('%Y-%m-%d %H:%M+%z')))
-
+    remove_duplicates()
     for line in keys:
         outfile.write("msgid \"{line}\"\n".format(line=line))
         outfile.write("msgstr \"\"\n\n")
+
+# this extracts the quest files from the quests directory
+#def get_quests():
+#    quests = set()
+#    infile = open('../quests/index.txt', 'r')
+#    for line in infile.readlines():
+#        quests.add(line.strip('\n'))
+#    infile.close()
+#    return quests
 
 extract('../items/items.txt')
 extract('../powers/powers.txt')
 for filename in os.listdir('../enemies'):
     extract('../enemies/{enemy}'.format(enemy=filename))
+#for filename in os.listdir('../maps'):
+#    extract('../maps/{map}'.format(map=filename))
+#for filename in get_quests():
+#    extract('../quests/{quest}'.format(quest=filename))
+#for filename in os.listdir('../npcs'):
+#    extract('../npcs/{npc}'.format(npc=filename))
 save('data.pot')
