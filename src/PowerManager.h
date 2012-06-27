68721043db9993d15cb51216e57b64e652f6d423
Justin Jacobs <justin@justinjacobs.com>
2012-06-27 14:10:53 -0400

    Make spawn and teleport powers target neighboring tiles

--- src/PowerManager.h
+++ src/PowerManager.h
@@ -181,6 +181,7 @@ struct Power {
 
 	// spawn info
 	std::string spawn_type;
+	bool target_neighbor;
 
 	Power() {
 		type = -1;
@@ -270,6 +271,7 @@ struct Power {
 
 		allow_power_mod = false;
 		spawn_type = "";
+		target_neighbor = false;
 	}
 
 };
@@ -298,6 +300,7 @@ private:
 
 	int calcDirection(int origin_x, int origin_y, int target_x, int target_y);
 	Point limitRange(int range, Point src, Point target);
+	Point targetNeighbor(Point target);
 	void initHazard(int powernum, StatBlock *src_stats, Point target, Hazard *haz);
 	void buff(int power_index, StatBlock *src_stats, Point target);
 	void playSound(int power_index, StatBlock *src_stats);
