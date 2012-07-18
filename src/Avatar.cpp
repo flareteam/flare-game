41486e68c89e7766660f766c5e2eb5843df61513
Justin Jacobs <jajdorkster@gmail.com>
2012-07-18 14:05:13 -0400

    Don't untransform player when on non-valid tile

--- src/Avatar.cpp
+++ src/Avatar.cpp
@@ -825,6 +825,8 @@ void Avatar::transform() {
 }
 
 void Avatar::untransform() {
+	// Only allow untransform when on a valid tile
+	if (!map->collider.valid_position(stats.pos.x,stats.pos.y,MOVEMENT_NORMAL)) return;
 
 	stats.transformed = false;
 	transform_triggered = false;
