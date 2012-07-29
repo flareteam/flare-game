906a5feab45ef27c9a90f8413a19b92a62cffcb8
Justin Jacobs <jajdorkster@gmail.com>
2012-07-29 15:51:40 -0400

    Larger tabs, readjust config menu

--- src/GameStateConfig.cpp
+++ src/GameStateConfig.cpp
@@ -67,6 +67,9 @@ GameStateConfig::GameStateConfig ()
 
 	// Initialize Widgets
 	tabControl = new WidgetTabControl(6);
+	tabControl->setMainArea(((VIEW_W - FRAME_W)/2)+3, (VIEW_H - FRAME_H)/2, FRAME_W, FRAME_H);
+	SDL_Rect frame = tabControl->getContentArea();
+
 	ok_button = new WidgetButton(mods->locate("images/menus/buttons/button_default.png"));
 	defaults_button = new WidgetButton(mods->locate("images/menus/buttons/button_default.png"));
 	cancel_button = new WidgetButton(mods->locate("images/menus/buttons/button_default.png"));
@@ -231,34 +234,34 @@ GameStateConfig::GameStateConfig ()
 					settings_lb[setting_num-1]->setY(y1);
 				} else if (setting_num < 47) {
 					// all labels except keybinding labels
-					settings_lb[setting_num-1]->setX((VIEW_W - FRAME_W)/2 + x1);
-					settings_lb[setting_num-1]->setY((VIEW_H - FRAME_H)/2 + y1);
+					settings_lb[setting_num-1]->setX(frame.x + x1);
+					settings_lb[setting_num-1]->setY(frame.y + y1);
 				}
 
 				if (setting_num < 10) {
 					//checkboxes positions
-					settings_cb[setting_num-1]->pos.x = (VIEW_W - FRAME_W)/2 + x2;
-					settings_cb[setting_num-1]->pos.y = (VIEW_H - FRAME_H)/2 + y2;
+					settings_cb[setting_num-1]->pos.x = frame.x + x2;
+					settings_cb[setting_num-1]->pos.y = frame.y + y2;
 				} else if ((setting_num > 9) && (setting_num < 13)) {
 					//sliders positions
-					settings_sl[setting_num-10]->pos.x = (VIEW_W - FRAME_W)/2 + x2;
-					settings_sl[setting_num-10]->pos.y = (VIEW_H - FRAME_H)/2 + y2;
+					settings_sl[setting_num-10]->pos.x = frame.x + x2;
+					settings_sl[setting_num-10]->pos.y = frame.y + y2;
 				} else if ((setting_num > 12) && (setting_num < 15)) {
 					//comboboxes positions
-					settings_cmb[setting_num-13]->pos.x = (VIEW_W - FRAME_W)/2 + x2;
-					settings_cmb[setting_num-13]->pos.y = (VIEW_H - FRAME_H)/2 + y2;
+					settings_cmb[setting_num-13]->pos.x = frame.x + x2;
+					settings_cmb[setting_num-13]->pos.y = frame.y + y2;
 				} else if (setting_num > 14 && setting_num < 40) {
 					//keybinding buttons positions
 					settings_key[setting_num-15]->pos.x = x2;
 					settings_key[setting_num-15]->pos.y = y2;
 				} else if (setting_num > 43 && setting_num < 47) {
 					//listboxes positions
-					settings_lstb[setting_num-44]->pos.x = (VIEW_W - FRAME_W)/2 + x2;
-					settings_lstb[setting_num-44]->pos.y = (VIEW_H - FRAME_H)/2 + y2;
+					settings_lstb[setting_num-44]->pos.x = frame.x + x2;
+					settings_lstb[setting_num-44]->pos.y = frame.y + y2;
 				} else if (setting_num > 46 && setting_num < 51) {
 					//mods config buttons positions
-					settings_btn[setting_num-47]->pos.x = (VIEW_W - FRAME_W)/2 + x1;
-					settings_btn[setting_num-47]->pos.y = (VIEW_H - FRAME_H)/2 + y1;
+					settings_btn[setting_num-47]->pos.x = frame.x + x1;
+					settings_btn[setting_num-47]->pos.y = frame.y + y1;
 				}
 			}
 
@@ -292,15 +295,12 @@ GameStateConfig::GameStateConfig ()
 
 	// Allocate KeyBindings ScrollBox
 	input_scrollbox = new WidgetScrollBox(scrollpane.w, scrollpane.h);
-	input_scrollbox->pos.x = scrollpane.x + (VIEW_W - FRAME_W)/2;
-	input_scrollbox->pos.y = scrollpane.y + (VIEW_H - FRAME_H)/2;
+	input_scrollbox->pos.x = scrollpane.x + frame.x;
+	input_scrollbox->pos.y = scrollpane.y + frame.y;
 	input_scrollbox->resize(780);
 	input_scrollbox->bg.r = scrollpane_color.x;
 	input_scrollbox->bg.g = scrollpane_color.y;
 	input_scrollbox->bg.b = scrollpane_color.w;
-
-	// Initialize the tab control.
-	tabControl->setMainArea(((VIEW_W - FRAME_W)/2)+3, (VIEW_H - FRAME_H)/2, FRAME_W, FRAME_H);
 
 	// Set positions of secondary key bindings
 	for (unsigned int i = 25; i < 50; i++) {
@@ -851,7 +851,7 @@ void GameStateConfig::logic ()
 		} else if (settings_btn[0]->checkClick()) {
 			settings_lstb[1]->shiftUp();
 		} else if (settings_btn[1]->checkClick()) {
-			settings_lstb[2]->shiftDown();
+			settings_lstb[1]->shiftDown();
 		} else if (settings_btn[2]->checkClick()) {
 			disableMods();
 		} else if (settings_btn[3]->checkClick()) {
