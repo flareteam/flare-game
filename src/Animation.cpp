1fa824aa732f697baaf2de7e778de88e1aa361ea
Stefan Beller <stefanbeller@googlemail.com>
2012-10-04 12:27:28 +0200

    Animation class: remove hardcoded direction number, rename to kinds.
    "direction" was very entity (enemy, hero) specific.
    "kind" is a more general naming and can also be used by i.e. powers in
    flare-game, which have 4 different animations, one to be picked at random.

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -42,6 +42,7 @@ Animation::Animation(const std::string &_name, const std::string &_type, SDL_Sur
 	, cur_frame(0)
 	, cur_frame_index(0)
 	, cur_frame_duration(0)
+	, max_kinds(0)
 	, additional_data(0)
 	, times_played(0)
 	, gfx(std::vector<SDL_Rect>())
@@ -60,6 +61,7 @@ Animation::Animation(const Animation& a)
 	, cur_frame(0)
 	, cur_frame_index(a.cur_frame_index)
 	, cur_frame_duration(a.cur_frame_duration)
+	, max_kinds(a.max_kinds)
 	, additional_data(a.additional_data)
 	, times_played(0)
 	, gfx(std::vector<SDL_Rect>(a.gfx))
@@ -68,24 +70,23 @@ Animation::Animation(const Animation& a)
 {
 }
 
-void Animation::setupUncompressed(Point _render_size, Point _render_offset, int _position, int _frames, int _duration) {
-	setup(_frames, _duration);
-
+void Animation::setupUncompressed(Point _render_size, Point _render_offset, int _position, int _frames, int _duration, unsigned short _maxkinds) {
+	setup(_frames, _duration, _maxkinds);
 
 	for (unsigned short i = 0 ; i < _frames; i++) {
-		int base_index = 8*i;
-		for (unsigned short direction = 0 ; direction < 8; direction++) {
-			gfx[base_index + direction].x = _render_size.x * (_position + i);
-			gfx[base_index + direction].y = _render_size.y * direction;
-			gfx[base_index + direction].w = _render_size.x;
-			gfx[base_index + direction].h = _render_size.y;
-			render_offset[base_index + direction].x = _render_offset.x;
-			render_offset[base_index + direction].y = _render_offset.y;
+		int base_index = max_kinds*i;
+		for (unsigned short kind = 0 ; kind < max_kinds; kind++) {
+			gfx[base_index + kind].x = _render_size.x * (_position + i);
+			gfx[base_index + kind].y = _render_size.y * kind;
+			gfx[base_index + kind].w = _render_size.x;
+			gfx[base_index + kind].h = _render_size.y;
+			render_offset[base_index + kind].x = _render_offset.x;
+			render_offset[base_index + kind].y = _render_offset.y;
 		}
 	}
 }
 
-void Animation::setup(unsigned short _frames, unsigned short _duration) {
+void Animation::setup(unsigned short _frames, unsigned short _duration, unsigned short _maxkinds) {
 	if (type == PLAY_ONCE) {
 		number_frames = _frames * _duration;
 		additional_data = 0;
@@ -99,32 +100,33 @@ void Animation::setup(unsigned short _frames, unsigned short _duration) {
 	cur_frame = 0;
 	cur_frame_index = 0;
 	cur_frame_duration = 0;
+	max_kinds = _maxkinds;
 	times_played = 0;
 
-	gfx.resize(8*_frames);
-	render_offset.resize(8*_frames);
-	duration.resize(8*_frames);
+	gfx.resize(max_kinds*_frames);
+	render_offset.resize(max_kinds*_frames);
+	duration.resize(max_kinds*_frames);
 	for (unsigned short i = 0; i < duration.size(); i++)
 		duration[i] = _duration;
 }
 
 void Animation::addFrame(	unsigned short index,
-							unsigned short direction,
+							unsigned short kind,
 							SDL_Rect sdl_rect,
 							Point _render_offset) {
 
-	if (index > gfx.size()/8) {
+	if (index > gfx.size()/max_kinds) {
 		fprintf(stderr, "WARNING: Animation(%s) adding rect(%d, %d, %d, %d) to frame index(%u) out of bounds. must be in [0, %d]\n",
-		name.c_str(), sdl_rect.x, sdl_rect.y, sdl_rect.w, sdl_rect.h, index, (int)gfx.size()/8);
+		name.c_str(), sdl_rect.x, sdl_rect.y, sdl_rect.w, sdl_rect.h, index, (int)gfx.size()/max_kinds);
 		return;
 	}
-	if (direction > 7) {
-		fprintf(stderr, "WARNING: Animation(%s) adding rect(%d, %d, %d, %d) to frame(%u) direction(%u) out of bounds. must be in [0, 7]\n",
-		name.c_str(), sdl_rect.x, sdl_rect.y, sdl_rect.w, sdl_rect.h, index, direction);
+	if (kind > max_kinds-1) {
+		fprintf(stderr, "WARNING: Animation(%s) adding rect(%d, %d, %d, %d) to frame(%u) kind(%u) out of bounds. must be in [0, %d]\n",
+		name.c_str(), sdl_rect.x, sdl_rect.y, sdl_rect.w, sdl_rect.h, index, kind, max_kinds-1);
 		return;
 	}
-	gfx[8*index+direction] = sdl_rect;
-	render_offset[8*index+direction] = _render_offset;
+	gfx[max_kinds*index+kind] = sdl_rect;
+	render_offset[max_kinds*index+kind] = _render_offset;
 }
 
 void Animation::advanceFrame() {
@@ -137,7 +139,7 @@ void Animation::advanceFrame() {
 
 	if (cur_frame_duration >= duration[cur_frame_index]) {
 		cur_frame_duration = 0;
-		unsigned short last_base_index = (gfx.size()/8)-1;
+		unsigned short last_base_index = (gfx.size()/max_kinds)-1;
 		switch(type) {
 		case PLAY_ONCE:
 
@@ -183,10 +185,10 @@ void Animation::advanceFrame() {
 	}
 }
 
-Renderable Animation::getCurrentFrame(int direction) {
+Renderable Animation::getCurrentFrame(int kind) {
 	Renderable r;
 
-	const int index = (8*cur_frame_index) + direction;
+	const int index = (max_kinds*cur_frame_index) + kind;
 	r.src.x = gfx[index].x;
 	r.src.y = gfx[index].y;
 	r.src.w = gfx[index].w;
