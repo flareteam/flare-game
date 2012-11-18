17e78dd1eea83bfaebea449d4f78d8df66bc581e
Stefan Beller <stefanbeller@googlemail.com>
2012-11-18 22:37:34 +0100

    Revert "Utils: replace preprocessor #if directive by if."
    This reverts commit 45a0492b0d07e5b449068ab65c005081079734cf.

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -239,18 +239,17 @@ void drawLine(SDL_Surface *screen, Point pos0, Point pos1, Uint32 color) {
 }
 
 void setSDL_RGBA(Uint32 *rmask, Uint32 *gmask, Uint32 *bmask, Uint32 *amask) {
-	// actually preprocessor not a variable:
-	if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-		*rmask = 0xff000000;
-		*gmask = 0x00ff0000;
-		*bmask = 0x0000ff00;
-		*amask = 0x000000ff;
-	} else {
-		*rmask = 0x000000ff;
-		*gmask = 0x0000ff00;
-		*bmask = 0x00ff0000;
-		*amask = 0xff000000;
-	}
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	*rmask = 0xff000000;
+	*gmask = 0x00ff0000;
+	*bmask = 0x0000ff00;
+	*amask = 0x000000ff;
+#else
+	*rmask = 0x000000ff;
+	*gmask = 0x0000ff00;
+	*bmask = 0x00ff0000;
+	*amask = 0xff000000;
+#endif
 }
 
 /**
