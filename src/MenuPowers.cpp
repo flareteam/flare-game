0d1ff820b338197b7eda98ddadc267c011b2f920
Justin Jacobs <jajdorkster@gmail.com>
2012-12-10 17:56:16 -0500

    Fix passive powers not being activated when unlocked

--- src/MenuPowers.cpp
+++ src/MenuPowers.cpp
@@ -373,6 +373,8 @@ bool MenuPowers::unlockClick(Point mouse) {
 					&& power_cell[i].requires_point && power_cell[i].tab == active_tab) {
 				stats->powers_list.push_back(power_cell[i].id);
 				stats->check_title = true;
+				// if the power is passive, activate it
+				powers->activateSinglePassive(stats, power_cell[i].id);
 				return true;
 			}
 		}
@@ -384,6 +386,8 @@ bool MenuPowers::unlockClick(Point mouse) {
 					&& points_left > 0 && power_cell[i].requires_point) {
 				stats->powers_list.push_back(power_cell[i].id);
 				stats->check_title = true;
+				// if the power is passive, activate it
+				powers->activateSinglePassive(stats, power_cell[i].id);
 				return true;
 			}
 		}
