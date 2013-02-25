156e72bd4da3c652c4fc09f1ab3332418c11a2e9
Henrik Andersson <hean01@users.sourceforge.net>
2013-02-25 22:25:55 +0100

    Fixes builderrors introduced in previous merge.

--- src/GameStatePlay.cpp
+++ src/GameStatePlay.cpp
@@ -574,7 +574,8 @@ void GameStatePlay::checkNPCInteraction() {
 	}
 
 	// if close enough to the NPC, open the appropriate interaction screen
-	if (npc_id != -1 && ((npc_click != -1 && interact_distance < max_interact_distance && pc->stats.alive && pc->stats.humanoid) || eventPendingDialog)) {
+	if (npc_id != -1 && ((npc_click != -1 && interact_distance < max_interact_distance && pc->statBlock()->alive && pc->statBlock()->humanoid) || eventPendingDialog)) {
+
 		if (inpt->pressing[MAIN1]) inpt->lock[MAIN1] = true;
 		if (inpt->pressing[ACCEPT]) inpt->lock[ACCEPT] = true;
 
@@ -599,7 +600,8 @@ void GameStatePlay::checkNPCInteraction() {
 		menu->npc->setNPC(NULL);
 	}
 
-	if (npc_id != -1 && ((interact_distance < max_interact_distance && pc->stats.alive && pc->stats.humanoid) || eventPendingDialog)) {
+	if (npc_id != -1 && ((interact_distance < max_interact_distance && pc->statBlock()->alive && pc->statBlock()->humanoid) || eventPendingDialog)) {
+
 
 		if (menu->talker->vendor_visible && !menu->vendor->talker_visible) {
 
