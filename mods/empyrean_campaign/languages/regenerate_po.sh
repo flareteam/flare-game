314a2c6302988d20d4bf9e8234ca8c40c0695b7c
Justin Jacobs <jajdorkster@gmail.com>
2018-04-09 14:12:56 -0400

    Update regenerate_po.sh from engine

--- mods/empyrean_campaign/languages/regenerate_po.sh
+++ mods/empyrean_campaign/languages/regenerate_po.sh
@@ -4,14 +4,17 @@
 # For the engine
 # To generate the appropriate .pot file, you need to run the following command from the languages directory:
 if [ -e engine.pot ] ; then
-xgettext --no-wrap --keyword=get -o engine.pot ../../../src/*.cpp
+xgettext --keyword=get -o engine.pot ../../../src/*.cpp
+
+# xgettext doesn't allow defining a charset, but we want UTF-8 across the board
+sed -i "s/charset=CHARSET/charset=UTF-8/" engine.pot
 
 # To update existing .po files, you need to run the following command from the languages directory:
-# msgmerge -U --no-wrap <name_of_.po_file> <name_of_.pot_file>
+# msgmerge -U -N <name_of_.po_file> <name_of_.pot_file>
 
 	for f in $(ls engine.*.po) ; do
 	echo "Processing $f"
-	msgmerge -U --no-wrap $f engine.pot
+	msgmerge -U -N $f engine.pot
 done
 fi
 
@@ -21,6 +24,6 @@ if [ -e data.pot ] ; then
 
 	for f in $(ls data.*.po) ; do
 		echo "Processing $f"
-		msgmerge -U --no-wrap $f data.pot
+		msgmerge -U -N $f data.pot
 	done
 fi
