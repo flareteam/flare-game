12be6ef84ea04789eb85e75d4bd165452ded5b07
Justin Jacobs <jajdorkster@gmail.com>
2021-05-23 20:33:48 -0400

    Update regenerate_po.sh from flare-engine

--- mods/empyrean_campaign/languages/regenerate_po.sh
+++ mods/empyrean_campaign/languages/regenerate_po.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Run this script in the language directory to update the pot and all *.po files
 
-DO_MERGE=true
+DO_MERGE=0
 for i in "$@"; do
-	if [ "$i" == "--no-merge" ]; then
-		DO_MERGE=false
+	if [ "$i" == "--do-merge" ]; then
+		DO_MERGE=1
 	fi
 done
 
@@ -18,7 +18,7 @@ if [ -e engine.pot ] ; then
 	# xgettext doesn't allow defining a charset, but we want UTF-8 across the board
 	sed -i "s/charset=CHARSET/charset=UTF-8/" engine.pot
 
-	if [ $DO_MERGE = true ]; then
+	if [ $DO_MERGE == 1 ]; then
 	# To update existing .po files, you need to run the following command from the languages directory:
 	# msgmerge -U -N <name_of_.po_file> <name_of_.pot_file>
 
@@ -35,7 +35,7 @@ if [ -e data.pot ] ; then
 	# For mods:
 	./xgettext.py
 
-	if [ $DO_MERGE = true ]; then
+	if [ $DO_MERGE == 1 ]; then
 		for f in $(ls data.*.po) ; do
 			echo "Processing $f"
 			msgmerge -U -N --backup=none $f data.pot
